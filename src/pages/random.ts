871e69d09c208f74883a0600f44052a4274fa40d
heftymouse <59918974+heftymouse@users.noreply.github.com>
2024-10-07 22:52:36 +0530

    retry if random site is the same

--- src/pages/random.ts
+++ src/pages/random.ts
@@ -4,6 +4,11 @@ import members from '@/members.json';
 export const prerender = false;
 
 export const GET: APIRoute = ({ request, url, redirect }) => {
-	const index = Math.floor(Math.random() * members.length);
+	const key = url.searchParams.get('site') ?? request.headers.get('Referer')
+	const currentIndex = members.findIndex((e) => e.website === key);
+	let index = -1;
+	do {
+		index = Math.floor(Math.random() * members.length);
+	} while(index === currentIndex); // this probably breaks horribly with only 1 member. we're cooler than that tho
 	return redirect(members[index].website, 302);
 };
