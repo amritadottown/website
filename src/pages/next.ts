f52398816f2a3371232e9a412f4f5a5794a4ac58
heftymouse <59918974+heftymouse@users.noreply.github.com>
2025-02-11 23:39:24 +0530

    ffs

--- src/pages/next.ts
+++ src/pages/next.ts
@@ -4,15 +4,15 @@ import members from '@/members.json';
 export const prerender = false;
 
 export const GET: APIRoute = ({ url, request, redirect }) => {
-	let key = url.searchParams.get('site') ?? request.headers.get('Referer');
+	let key = url.searchParams.get('site') ?? new URL(request.headers.get('Referer')).host;
 
 	if (key) {
 		const index = members.findIndex((e) => e.website === key);
 		if (index !== -1) {
-			const prev = index === members.length - 1 ? 0 : index + 1;
-			return redirect(`https://${members[prev].website}/`, 302);
+			const next = index === members.length - 1 ? 0 : index + 1;
+			return redirect(`https://${members[next].website}/`, 302);
 		}
 	}
-	
+
 	return redirect('/', 302);
 };
--- src/pages/random.ts
+++ src/pages/random.ts
@@ -4,12 +4,12 @@ import members from '@/members.json';
 export const prerender = false;
 
 export const GET: APIRoute = ({ request, url, redirect }) => {
-	const key = url.searchParams.get('site') ?? request.headers.get('Referer')
+	const key = url.searchParams.get('site') ?? new URL(request.headers.get('Referer')).host;
 	const currentIndex = members.findIndex((e) => e.website === key);
 	let index = -1;
 	do {
 		index = Math.floor(Math.random() * members.length);
 	} while(index === currentIndex); // this probably breaks horribly with only 1 member. we're cooler than that tho
-	
+
 	return redirect(`https://${members[index].website}/`, 302);
 };
